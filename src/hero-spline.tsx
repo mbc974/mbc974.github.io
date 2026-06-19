da5fb1036d2f46b7c8d5187ec37bce564acccf7e
MBC974 <mbc.re974@gmail.com>
2026-06-19 12:38:54 +0400

    yo

--- src/hero-spline.tsx
+++ src/hero-spline.tsx
@@ -1,7 +1,7 @@
 import { SplineScene } from "./components/ui/spline-scene";
 
 // Coller ici l'URL .splinecode de la scene Spline basket MBC.
-const MBC_SPLINE_SCENE_URL = "";
+const MBC_SPLINE_SCENE_URL = "https://prod.spline.design/KHuEbIVgmKJCZVLk/scene.splinecode";
 
 export function HeroSpline() {
   if (!MBC_SPLINE_SCENE_URL) return null;
