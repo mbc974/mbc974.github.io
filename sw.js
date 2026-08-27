a11b6f9ff849a8b3bc9c6802f3f942234899a023
MBC974 <mbc.re974@gmail.com>
2026-08-27 19:08:11 +0400

    ert

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    ⚠️  Bump le nom du cache (mbc-vN) à chaque déploiement
        important pour purger l'ancien contenu.
    ============================================================ */
-const CACHE = 'mbc-9abc365c-6f934943';
+const CACHE = 'mbc-7d73d764-6f934943';
 const OFFLINE_URL = '/offline.html';
 const PRECACHE = [
   '/',
@@ -13,7 +13,7 @@ const PRECACHE = [
   '/adhesion.html',
   '/offline.html',
   '/site.webmanifest',
-  '/style.css?v=9abc365c',
+  '/style.css?v=7d73d764',
   '/script.js?v=6f934943',
   '/assets/logos/mbc-logo.webp',
   '/assets/icons/favicon.png',
